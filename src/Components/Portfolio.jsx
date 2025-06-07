98cd422a40ccedaed1fb436574609a803969d951
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-06-07 05:10:47 +0000

    changed content

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,29 +31,19 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Agile Project Management Dashboard",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A dashboard built with python and Flask to manage Agile projects. It allows users to create, update, and track tasks, sprints, and team members.",
+    url: "https://agilemgmtdashboard.onrender.com/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Secure Login and Logout System",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A secure login and logout system built with jsp and servlets. It includes user authentication, session management, and password encryption.",
+    url: "https://github.com/PranavNag2005/Secure-login-system-java",
   },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+
+  
 ];
 
 const Portfolio = () => {
